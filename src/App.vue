23784b839ecbee5e4a181294c854282c2cafe0ed
precioussilence <deepautumnfeelings@gmail.com>
2025-11-16 18:22:06 +0800

    实现一个“姓名输入框”，绑定到 ref，并在页面显示

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NameInput from './components/NameInput.vue'
+</script>
 
 <template>
   <h1>Hello Vue 3!</h1>
@@ -6,6 +8,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <NameInput></NameInput>
 </template>
 
 <style scoped></style>
---
+++ src/components/NameInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input type="text" placeholder="请输入你的姓名" v-model="name" />
+    <p>姓名：{{ name }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
+
+<style scoped></style>
