76a140588f93207288489ec4b1912303277fde18
precioussilence <deepautumnfeelings@gmail.com>
2025-11-16 20:40:01 +0800

    给 Todo List 加上：数据持久化（刷新不丢）、删除时加确认（用 useConfirmDialog）

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -11,19 +11,36 @@
       <li v-for="(value, index) in todos" :key="index">
         <span>{{ value }}</span>
         <button @click="modify(index, todo)">修改</button>
-        <button @click="remove(index)">删除</button>
+        <button @click="handleRemove(index)">删除</button>
       </li>
     </ul>
+    <div v-if="isRevealed">
+      <h2>确定要删除这条待办吗？</h2>
+      <button @click="confirm">确定</button>
+      <button @click="cancel">取消</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useTodoStore } from '@/stores/todo'
+import { useConfirmDialog, useLocalStorage } from '@vueuse/core'
 import { ref } from 'vue'
 
 const todo = ref('')
 
 const { todos, add, modify, remove } = useTodoStore()
+
+useLocalStorage('todo', todo)
+
+const { isRevealed, reveal, confirm, cancel } = useConfirmDialog()
+
+async function handleRemove(index: number) {
+  const result = await reveal()
+  if (!result.isCanceled) {
+    remove(index)
+  }
+}
 </script>
 
 <style scoped></style>
--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -1,17 +1,17 @@
+import { useLocalStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
 
 export const useTodoStore = defineStore('todo', () => {
-  const todos = reactive<string[]>([])
+  const todos = useLocalStorage<string[]>('todos', [])
 
   function add(value: string) {
-    todos.push(value)
+    todos.value.push(value)
   }
   function modify(index: number, value: string) {
-    todos[index] = value
+    todos.value[index] = value
   }
   function remove(index: number) {
-    todos.splice(index, 1)
+    todos.value.splice(index, 1)
   }
   return { todos, add, modify, remove }
 })
