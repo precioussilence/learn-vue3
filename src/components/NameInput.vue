d17947d16d3b2d2bd6a0a9cd9602db0bf718d2cc
precioussilence <deepautumnfeelings@gmail.com>
2025-11-16 18:34:55 +0800

    在姓名输入基础上，加一个“全名”显示（用 computed），并用 watch 打印日志

--- src/components/NameInput.vue
+++ src/components/NameInput.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <input type="text" placeholder="请输入你的姓名" v-model="name" />
-    <p>姓名：{{ name }}</p>
+    <input type="text" placeholder="请输入你的名字" v-model="first_name" />
+    <input type="text" placeholder="请输入你的姓氏" v-model="last_name" />
+    <p>你的全名是：{{ full_name }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
-const name = ref('')
+const first_name = ref('')
+const last_name = ref('')
+
+const full_name = computed(() => `${first_name.value} ${last_name.value}`)
+watch(full_name, (newVal, oldVal) => {
+  console.log('姓名已变化！')
+  console.log(`旧：${oldVal}`, `新：${newVal}`)
+})
 </script>
 
 <style scoped></style>
