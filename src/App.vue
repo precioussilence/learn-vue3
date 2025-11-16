7b9ce45bb99fcff9be45f63c5dc8534381e59b54
precioussilence <deepautumnfeelings@gmail.com>
2025-11-16 18:04:04 +0800

    修改 App.vue，让它显示 “Hello, Vue 3!”

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Hello Vue 3!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
